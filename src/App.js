242d2d8f863faa1cf4b6bd5ee91e474192608e7c
Tribhuwan <mathincosmos@gmail.com>
2022-11-30 13:29:34 +0530

    set initial data state

--- src/App.js
+++ src/App.js
@@ -12,20 +12,21 @@ export default class App extends Component {
           name: "Your Name",
           profession: "Your profession",
           phone: "+98 1234352",
-          emial: "yourCoolemail@gmail.com",
-          linekdin: "https://linkedin.com/cool",
+          email: "yourCoolemail@gmail.com",
+          linkedin: "https://linkedin.com/cool",
         },
         edu: {
           college: "Your college",
           degree: "Your degree",
-          from: "12-08-12",
-          to: "12/08/2005",
+          from: "2015",
+          to: "2020",
         },
         exp: {
           company: "Intellects",
           pos: "Junior Data Scientist",
-          from: "12/02/2008",
-          tasks:"Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa egestas mollis varius; dignissim elementum. Mollis tincidunt mattis hendrerit dolor eros enim, nisi ligula ornare. Hendrerit parturient habitant pharetra rutrum gravida porttitor eros feugiat. Mollis elit sodales taciti duis praesent id. Consequat urna vitae morbi nunc congue."
+          from: "2012",
+          to: "2030",
+          task:"All the React components can have a state associated with them. The state of a component can change either due to a response to an action performed by the user or an event triggered by the system. Whenever the state changes, React re-renders the component to the browser. Before updating the value of the state, we need to build an initial state setup. Once we are done with it, we use the setState() method to change the state object. It ensures that the component has been updated and calls for re-rendering of the component.setState is asynchronous call means if synchronous call get called it may not get updated at right time like to know current value of object after update using setState it may not get give current updated value on console. To get some behavior of synchronous need to pass function instead of object to setState."
         },
         
       },
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,34 +2,31 @@ import React, { Component } from "react";
 
 class EducationData extends Component {
   render() {
+    const edu = this.props.edu;
     return (
       <>
         <div className="edu--degree font-semibold">
-          Bachelor of Computer Application
+          {edu.degree}
         </div>
         <div className="edu--college text-sm">
-          National Institute of Intellects
+          {edu.college}
         </div>
-        <div className="edu--date text-sm">2018 - 2022</div>
+        <div className="edu--date text-sm">{edu.from} - {edu.to}</div>
       </>
     );
   }
 }
 class ExperienceData extends Component {
   render() {
+    const exp = this.props.exp;
     return (
       <>
-        <div className="exp--pos font-semibold">Junior Data Scientist</div>
-        <div className="exp--company">Intellects</div>
-        <div className="exp--date text-sm">2023 - 2028</div>
+        <div className="exp--pos font-semibold">{exp.pos}</div>
+        <div className="exp--company">{exp.company}</div>
+        <div className="exp--date text-sm">{exp.from} - {exp.to}</div>
         <div className="mt-1 font-semibold underline">Task</div>
-        <div className="exp--work h-[50%] w-[90%] overflow-y-auto text-sm word-wrap">
-          Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa
-          egestas mollis varius; dignissim elementum. Mollis tincidunt mattis
-          hendrerit dolor eros enim, nisi ligula ornare. Hendrerit parturient
-          habitant pharetra rutrum gravida porttitor eros feugiat. Mollis elit
-          sodales taciti duis praesent id. Consequat urna vitae morbi nunc
-          congue.
+        <div className="exp--work h-[100px] w-[80%] overflow-y-auto text-sm word-wrap">
+         {exp.task}
         </div>
       </>
     );
@@ -37,26 +34,27 @@ class ExperienceData extends Component {
 }
 class ContactData extends Component {
   render() {
+    const personal = this.props.personal
     return (
       <>
         <div className="linkedin">
           <span className="text-sm font-semibold mr-4">Linkedin</span>{" "}
           <a
-            href="www.youtube"
+            href={personal.linkedin}
             className="font-italic underline text-blue-600 text-sm"
             target="_blank"
             rel="noopener noreferrer"
           >
-            https://linkedin.com/cool
+            {personal.linkedin}
           </a>
         </div>
         <div className="email">
           <span className="font-semibold mr-4">Email</span>{" "}
-          <span className="text-sm ">dripfullemail@outlook.com</span>
+          <span className="text-sm ">{personal.email}</span>
         </div>
         <div className="Phone">
           <span className="text-sm font-semibold mr-4">Phone</span>{" "}
-          <span className="text-sm ">+23 2332312343</span>
+          <span className="text-sm ">{personal.phone}</span>
         </div>
       </>
     );
@@ -64,13 +62,14 @@ class ContactData extends Component {
 }
 class RightSection extends Component {
   render() {
+    const { personal, edu, exp } = this.props.data;
     return (
       <section className="bg-[#D8D9CF] flex justify-center items-center">
         <div className="h-[90%] shadow-md overflow-y-auto  w-[60%] bg-white ">
           <header className="h-[12%] bg-[#947EC3] text-white flex p-2 items-center">
             <div className="right--nameInfo">
-              <div className="name text-3xl">Tribhuwan Joshi</div>
-              <div className="profession">Senior Research Scientist</div>
+              <div className="name text-3xl">{personal.name}</div>
+              <div className="profession">{personal.profession}</div>
             </div>
           </header>
           <main className="p-1 space-y-2 px-4">
@@ -78,21 +77,21 @@ class RightSection extends Component {
               <div className="text-2xl font-sans  font-semibold underline-offset-2 underline mt-2 mb-1">
                 Education
               </div>
-              <EducationData />
+              <EducationData edu={edu} />
             </div>
             <hr className="bg-black h-[2px] border-b-0" />
             <div className="experience h-full w-full">
               <div className="text-2xl font-sans  font-semibold underline-offset-2 underline mt-2 mb-1">
                 Experience
               </div>
-              <ExperienceData />
+              <ExperienceData exp={exp} />
             </div>
             <hr className="bg-black h-[2px] border-b-0" />
             <div className="contact -mt-10">
               <div className="text-2xl font-sans  font-semibold underline-offset-2 mb-2 underline">
                 Contact
               </div>
-              <ContactData />
+              <ContactData personal={personal} />
             </div>
           </main>
         </div>
@@ -178,6 +177,7 @@ class EducationForm extends Component {
 }
 class ExperienceForm extends Component {
   render() {
+
     return (
       <fieldset className="border-[1px] p-4  border-black">
         <legend className="text-center font-semibold">Experience</legend>
@@ -212,9 +212,9 @@ class ExperienceForm extends Component {
               <input type="date" id="work-to" required />
             </div>
           </div>
-          <div className="w-[100%] h-[50px] bg-red-500">
+          <div className="w-[100%] h-[50px]">
             <textarea
-              className="border-gray-700 w-full h-full text-sm outline-none border"
+              className="border-gray-700 w-full h-[full] text-sm outline-none border"
               placeholder=" Describe your task briefly"
             ></textarea>
           </div>
@@ -254,11 +254,12 @@ class LeftSection extends Component {
 
 export default class Main extends Component {
   render() {
-    const { click } = this.props;
+    const { click, data } = this.props;
+
     return (
       <main className="flex-1 grid grid-cols-[repeat(2,minmax(700px,1fr))]">
-        <LeftSection click={click} />
-        <RightSection />
+        <LeftSection data={data} click={click} />
+        <RightSection data={data} />
       </main>
     );
   }
